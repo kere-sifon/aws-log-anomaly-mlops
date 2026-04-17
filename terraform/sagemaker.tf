632d5b65ad0ca0fc2515a88f4668b016d225edfd
Kere Ekpenyong <Kere.Ekpenyong@ibm.com>
2026-04-17 17:37:45 -0400

    Update sagemaker.tf

--- terraform/sagemaker.tf
+++ terraform/sagemaker.tf
@@ -3,18 +3,18 @@
 
 data "aws_sagemaker_prebuilt_ecr_image" "sklearn_inference" {
   repository_name = "sagemaker-scikit-learn"
-  image_tag       = var.sklearn_inference_image_tag
+  image_tag      = var.sklearn_inference_image_tag
 }
 
 resource "aws_sagemaker_model" "log_anomaly_model" {
-  name               = "log-anomaly-model"
+  name              = "log-anomaly-model"
   execution_role_arn = aws_iam_role.sagemaker_execution.arn
 
   primary_container {
     image          = data.aws_sagemaker_prebuilt_ecr_image.sklearn_inference.registry_path
     mode           = "SingleModel"
     model_data_url = "s3://${aws_s3_bucket.model_artifacts.bucket}/${aws_s3_object.bootstrap_model_artifact.key}"
-    environment = {
+    environment    = {
       # Module name for code/inference.py (no .py suffix; importlib.import_module).
       SAGEMAKER_PROGRAM = "inference"
     }
@@ -31,13 +31,13 @@ resource "aws_sagemaker_endpoint_configuration" "log_anomaly_endpoint_config" {
   depends_on = [aws_sagemaker_model.log_anomaly_model]
 
   production_variants {
-    variant_name                                      = "primary"
-    model_name                                        = aws_sagemaker_model.log_anomaly_model.name
-    initial_instance_count                            = 1
-    instance_type                                     = "ml.t2.medium"
-    initial_variant_weight                            = 1
+    variant_name                                     = "primary"
+    model_name                                       = aws_sagemaker_model.log_anomaly_model.name
+    initial_instance_count                           = 1
+    instance_type                                    = "ml.t2.medium"
+    initial_variant_weight                           = 1
     # Defaults are short; first-time image pull + model load can exceed them and surface as ping failures.
-    model_data_download_timeout_in_seconds            = 900
+    model_data_download_timeout_in_seconds           = 900
     container_startup_health_check_timeout_in_seconds = 900
   }
 
@@ -45,9 +45,9 @@ resource "aws_sagemaker_endpoint_configuration" "log_anomaly_endpoint_config" {
 }
 
 resource "aws_sagemaker_endpoint" "log_anomaly_detector" {
-  name                 = "log-anomaly-detector-endpoint"
+  name                = "log-anomaly-detector-endpoint"
   endpoint_config_name = aws_sagemaker_endpoint_configuration.log_anomaly_endpoint_config.name
-  tags                 = local.default_tags
+  tags                = local.default_tags
 
   depends_on = [
     aws_sagemaker_model.log_anomaly_model,
